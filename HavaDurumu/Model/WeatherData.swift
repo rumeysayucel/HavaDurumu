3b5c7521f2c127216f410b4f3cce2890c6acd310
Rumeysa Yücel <rumeysa.yucel.1996@gmail.com>
2021-07-06 15:34:04 +0300

    Initial Commit

---
+++ HavaDurumu/Model/WeatherData.swift
@@ -0,0 +1,22 @@
+//
+//  WeatherData.swift
+//  HavaDurumu
+//
+//  Created by Rumeysa Yücel on 5.07.2021.
+//
+
+import Foundation
+
+struct WeatherData: Codable {
+    let name: String
+    let main : Main
+    let weather : [Weather]
+}
+
+struct Main: Codable {
+    let temp: Double
+}
+
+struct Weather: Codable {
+    let id :Int
+}
